0c8dd471b382ee647afa37bf6b00ec99c6b1faed
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-06-21 20:30:37 -0400

    6-21-2020

--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -9,65 +9,71 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzTest {
 
     @Test
-    public void createFizzBuzzClass(){
+    public void createFizzBuzzClass() {
         FizzBuzz underTest = new FizzBuzz();
     }
 
     @Test
-    public void oneShouldSay1(){
-        //arrangement
+    public void oneShouldSay1() {
+
         FizzBuzz underTest = new FizzBuzz();
-        //action
+
         String result = underTest.say(1);
-        //assertion
+
         assertEquals("1", result);
     }
 
     @Test
-    public void twoShouldSay2(){
+    public void twoShouldSay2() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(2);
         assertEquals("2", result);
     }
 
     @Test
-    public void threeShouldSayFizz(){
+    public void threeShouldSayFizz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(3);
         assertEquals("Fizz", result);
     }
+
     @Test
-    public void fourShouldSay4(){
+    public void fourShouldSay4() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(4);
         assertEquals("4", result);
     }
+
     @Test
-    public void fiveShouldSayBuzz(){
+    public void fiveShouldSayBuzz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(5);
         assertEquals("Buzz", result);
     }
+
     @Test
-    public void sixShouldSayFizz(){
+    public void sixShouldSayFizz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(6);
         assertEquals("Fizz", result);
     }
+
     @Test
-    public void nineShouldSayFizz(){
+    public void nineShouldSayFizz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(9);
         assertEquals("Fizz", result);
     }
+
     @Test
-    public void tenShouldSayBuzz(){
+    public void tenShouldSayBuzz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(10);
         assertEquals("Buzz", result);
     }
+
     @Test
-    public void fifteenShouldSayFizzBuzz(){
+    public void fifteenShouldSayFizzBuzz() {
         FizzBuzz underTest = new FizzBuzz();
         String result = underTest.say(15);
         assertEquals("FizzBuzz", result);
