16284d060a66531efe541c0cd68044c953828feb
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-06-21 20:28:41 -0400

    6-21-2020  fizz clean up

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,18 @@
 package org.wecancodeit.fizzbuzz;
 
-
 public class FizzBuzz {
+
+    public String say(int numberToTranslate) {
+        if (numberToTranslate % 3 == 0 && numberToTranslate % 5 == 0) {
+            return "FizzBuzz";
+        }
+        if (numberToTranslate % 3 == 0) {
+            return "Fizz";
+        }
+        if (numberToTranslate % 5 == 0) {
+            return "Buzz";
+        }
+        return "" + numberToTranslate;
+    }
+
 }
